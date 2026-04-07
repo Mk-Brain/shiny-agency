9c70fd78aacfc25f22f9d2cd015f336496b68e9d
mkbrain <morandkentsa9@gmail.com>
2026-04-07 10:43:19 +0200

    manipulation des outlets sur les questionnaire

--- src/pages/Survey.tsx
+++ src/pages/Survey.tsx
@@ -4,6 +4,7 @@ function Survey() {
     return (
         <div>
             <h1>Questionnaire 🧮</h1>
+            <h2>Utilisation des Outlets pour afficher differents formulaire</h2>
             <Link to="Client">Questionnaire Client</Link><br />
             <Link to="Freelance">Questionnaire Freelance</Link>
             <Outlet/>
